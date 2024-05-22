bfc9c822f6b5714bf013205b7abd1a66353dc1af
huu3323 <143768334+huu3323@users.noreply.github.com>
2024-05-23 01:28:36 +0800

    Update py.py

--- py.py
+++ py.py
@@ -68,7 +68,7 @@ else:
 
     if __name__ == "__main__":
         min_steps = 10000
-        max_steps = 60000
+        max_steps = 30000
 
         for account, password in account_password_pairs:
             result = modify_steps(account, password, min_steps, max_steps)
